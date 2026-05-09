04734fb5a52fc4b31ecdc5a53e34fe388f943397
BarbaraaIC <antonia.inostroza711@gmail.com~>
2026-05-08 23:29:17 -0400

    subiendo

---
+++ backend/src/config/configEnv.js
@@ -0,0 +1,5 @@
+export const HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || 5432;
+export const DB_USERNAME = process.env.DB_USER || 'postgres';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '1234';
+export const DATABASE = process.env.DB_NAME || 'tesis-db';
